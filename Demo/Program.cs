4d5bdc4b9dfbea59a4c12b46ad1440ef23ae8c52
KaiTsui <francise4237@hotmail.com>
2021-01-02 20:48:42 +0800

    First Add

---
+++ Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test();
+        }
+
+        static void test()
+        {
+            JsonConfig.JsonConfig cfg = new JsonConfig.JsonConfig("..\\config.json");
+            dynamic aaa = cfg.GetProperty("AAA");
+            Console.WriteLine(aaa.Name);
+            Console.ReadLine();
+            aaa.Name = "Andy";
+            aaa.Addr = "BBB";
+            aaa.C = "CCC";
+            aaa.array = new object[] { "arr", 333, DateTime.Now };
+            cfg.Save();
+        }
+    }
+}
---
+++ JsonConfig/JsonConfig.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JsonConfig
+{
+    public class JsonConfig
+    {
+        public string Json { get; private set; }
+        private IDictionary<string, object> props;
+        private string JsonFile;
+
+        public JsonConfig(string jsonFile)
+        {
+            JsonFile = jsonFile;
+            Json = File.Exists(jsonFile) ? File.ReadAllText(jsonFile) : "{}";
+            props = JsonConvert.DeserializeObject<ExpandoObject>(Json, new ExpandoObjectConverter()) as IDictionary<string, Object>;
+        }
+
+        public ExpandoObject GetProperty(string propName)
+        {
+            dynamic prop;
+            if (!props.TryGetValue(propName, out prop))
+            {
+                prop = new ExpandoObject();
+                props.Add(propName, prop);
+            }
+            return prop;
+        }
+
+        public IDictionary<string, Object> Save()
+        {
+            var dir = Path.GetDirectoryName(JsonFile);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            File.WriteAllText(JsonFile, JsonConvert.SerializeObject(props, Formatting.Indented));
+            return props;
+        }
+    }
+}
